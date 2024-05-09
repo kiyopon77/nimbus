501cd95ccab8d774f3d8abbbdef5686cb6b2e3a1
Rishabh Singh <89176164+kiyopon77@users.noreply.github.com>
2024-05-10 01:30:11 +0530

    Update main0.py

--- main0.py
+++ main0.py
@@ -2,7 +2,7 @@
 import requests
 
 # OpenWeatherMap API key
-api_key = '2a22ee2d87b09d70b65a25d5661f2f00'
+api_key = 'Your_API_Key'
 
 city = 'Tokyo'
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
